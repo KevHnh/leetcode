35994ce7c7104a66c8936973c84e46d3339813fb
Kevin Hinh <kevinhinh9@gmail.com>
2023-08-10 23:37:26 -0400

    Time: 54 ms (65.09%) | Memory: 42.6 MB (9.59%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.js
@@ -14,16 +14,16 @@ var search = function(nums, target) {
             return mid
         }
 
-        if (nums[left] <= nums[mid]) { //left sorted
-            if (target > nums[mid] || target < nums[left]) {
+        if (nums[left] <= nums[mid]) {
+            if (target < nums[left] || target > nums[mid]) {
                 left = mid + 1
             }
             else {
                 right = mid - 1
             }
         }
-        else { // right sorted
-            if (target < nums[mid] || target > nums[right]) {
+        else {
+            if (target > nums[right] || target < nums[mid]) {
                 right = mid - 1
             }
             else {
