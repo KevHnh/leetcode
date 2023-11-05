b598872c39e3a54a6b3c8aae1f4e92b2af7289b9
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-05 18:52:32 -0500

    Time: 66 ms (74.32%) | Memory: 42.3 MB (91.87%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+        return false
+    }
+
+    let map = {}
+
+    for (let i = 0; i < s.length; i++) {
+        map[s[i]] ? map[s[i]]++ : map[s[i]] = 1
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (map[t[i]] > 0) {
+            map[t[i]]--
+        }
+        else {
+            return false
+        }
+    }
+
+    return true
+};
