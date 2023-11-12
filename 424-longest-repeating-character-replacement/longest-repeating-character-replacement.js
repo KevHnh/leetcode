098e1f40ec8c998d410aadc9c0a1ebd893e52cc6
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-12 17:41:19 -0500

    Time: 71 ms (57.07%) | Memory: 42.4 MB (72.84%) - LeetSync

---
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {
+    let map = {}
+    let left = 0
+    let right = 0
+    let maxChar = 0
+    let ans = 0
+
+    while (right < s.length) {
+        map[s[right]] ? map[s[right]]++ : map[s[right]] = 1
+        maxChar = Math.max(maxChar, map[s[right]])
+
+        while ((right - left + 1) - maxChar > k) {
+            map[s[left]]--
+            left++
+        }
+
+        ans = Math.max(ans, right - left + 1)
+        right++
+    }
+
+    return ans
+};
