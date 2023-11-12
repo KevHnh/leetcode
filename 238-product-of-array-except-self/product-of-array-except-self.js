0d6b3532ba97f600a7f54533121c85146079533f
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-12 17:23:24 -0500

    Time: 81 ms (82.35%) | Memory: 54.9 MB (42.12%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    let product = 1
+    let ans = []
+
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = product
+        product *= nums[i]
+    }
+
+    product = 1
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        ans[i] *= product
+        product *= nums[i]
+    }
+
+    return ans
+};
