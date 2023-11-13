28c5434070ef033fd3a00ea2d51b039fbe5a166e
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-12 22:17:17 -0500

    Time: 70 ms (92.42%) | Memory: 46.8 MB (47.03%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.js
+++ 76-minimum-window-substring/minimum-window-substring.js
@@ -3,17 +3,17 @@
  * @param {string} t
  * @return {string}
  */
-var minWindow = function (s, t) {
+var minWindow = function(s, t) {
     let map = {}
+    let left = 0
+    let right = 0
+    let ans = ""
 
     for (let i = 0; i < t.length; i++) {
         map[t[i]] ? map[t[i]]++ : map[t[i]] = 1
     }
 
-    let left = 0
-    let right = 0
     let reqLen = Object.keys(map).length
-    let ans = ""
 
     while (right < s.length) {
         map[s[right]]--
@@ -24,7 +24,7 @@ var minWindow = function (s, t) {
 
         while (reqLen === 0) {
             if (!ans || ans.length > right - left + 1) {
-                ans = s.slice(left, right + 1)
+                ans = s.substring(left, right + 1)
             }
 
             if (map[s[left]] === 0) {
@@ -38,5 +38,5 @@ var minWindow = function (s, t) {
         right++
     }
 
-    return ans;
+    return ans
 };
