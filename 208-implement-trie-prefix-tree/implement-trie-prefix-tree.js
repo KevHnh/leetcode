10f0746bcb75d9366babca03000c3b6b5602303e
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-14 16:24:51 -0500

    Time: 180 ms (22.88%) | Memory: 65.8 MB (37.80%) - LeetSync

---
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.js
@@ -0,0 +1,55 @@
+class TrieNode {
+    constructor() {
+        this.children = {}
+        this.isWord = false
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode()
+    }
+
+    insert(word) {
+        let node = this.root
+
+        for (let char of word) {
+            node.children[char] = node.children[char] || new TrieNode()
+            node = node.children[char]
+        }
+
+        node.isWord = true
+    }
+
+    search(word) {
+        let node = this.root
+
+        for (let char of word) {
+            node.children[char] = node.children[char] || null
+
+            if (!node.children[char]) {
+                return false
+            }
+
+            node = node.children[char]
+        }
+
+        return node.isWord
+    }
+
+    startsWith(prefix) {
+        let node = this.root
+
+        for (let char of prefix) {
+            node.children[char] = node.children[char] || null
+
+            if (!node.children[char]) {
+                return false
+            }
+
+            node = node.children[char]
+        }
+
+        return true
+    }
+}
