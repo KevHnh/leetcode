09027ae3fff253b28daed52426f7e34c797e876a
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-15 23:56:36 -0500

    Time: 113 ms (49.30%) | Memory: 50.9 MB (64.92%) - LeetSync

---
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} heights
+ * @return {number[][]}
+ */
+var pacificAtlantic = function(heights) {
+    let ROWS = heights.length
+    let COLS = heights[0].length
+    let pac = new Set()
+    let atl = new Set()
+    let res = []
+
+    function dfs(r, c, visit, prevHeight) {
+        if (Math.min(r, c) < 0 || r >= ROWS || c >= COLS || visit.has(r + "-" + c) || heights[r][c] < prevHeight) {
+            return
+        }
+
+        visit.add(r + "-" + c)
+        dfs(r + 1, c, visit, heights[r][c])
+        dfs(r - 1, c, visit, heights[r][c])
+        dfs(r, c + 1, visit, heights[r][c])
+        dfs(r, c - 1, visit, heights[r][c])
+    }
+
+    for (let r = 0; r < ROWS; r++) {
+        dfs(r, 0, pac, heights[r][0])
+        dfs(r, COLS - 1, atl, heights[r][COLS - 1])
+    }
+
+    for (let c = 0; c < COLS; c++) {
+        dfs(0, c, pac, heights[0][c])
+        dfs(ROWS - 1, c, atl, heights[ROWS - 1][c])
+    }
+
+    for (let i = 0; i < ROWS; i++) {
+        for (let j = 0; j < COLS; j++) {
+            if (pac.has(i + "-" + j) && atl.has(i + "-" + j) ) {
+                res.push([i, j])
+            }
+        }
+    }
+
+    return res
+};
