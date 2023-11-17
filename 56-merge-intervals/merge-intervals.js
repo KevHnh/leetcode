5f3e9edc1a37ac0a941687a28bddf0e95a61b03f
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-16 23:57:15 -0500

    Time: 86 ms (78.81%) | Memory: 48.2 MB (93.19%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    intervals = intervals.sort((a,b) => a[0] - b[0])
+    let res = [intervals[0]]
+
+    for (let i = 1; i < intervals.length; i++) {
+        let prev = res[res.length - 1]
+
+        if (prev[1] >= intervals[i][0]) {
+            prev[1] = Math.max(intervals[i][1], prev[1])
+        }
+        else {
+            res.push(intervals[i])
+        }
+    }
+
+    return res
+};
