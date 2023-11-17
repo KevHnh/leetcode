3ee539ad1ff49815969f7b3f6124badba48fdd3b
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-16 23:34:20 -0500

    Time: 61 ms (70.62%) | Memory: 43.9 MB (84.76%) - LeetSync

---
+++ 57-insert-interval/insert-interval.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+var insert = function(intervals, newInterval) {
+    let res = []
+
+    for (let i = 0; i < intervals.length; i++) {
+        if (intervals[i][0] > newInterval[1]) {
+            res.push(newInterval)
+            return res.concat(intervals.slice(i))
+        }
+        else if (intervals[i][1] < newInterval[0]) {
+            res.push(intervals[i])
+        }
+        else {
+            newInterval = [Math.min(intervals[i][0], newInterval[0]), Math.max(intervals[i][1], newInterval[1])]
+        }
+    }
+
+    res.push(newInterval)
+    return res
+};
