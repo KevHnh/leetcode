76a9a42d33a236f65c80654d1cbe0dbd528ff3ab
Kevin Hinh <kevinhinh9@gmail.com>
2023-11-22 17:24:29 -0500

    Time: 58 ms (16.72%) | Memory: 41.1 MB (98.73%) - LeetSync

---
+++ 198-house-robber/house-robber.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    let memo = new Array(nums.length + 1)
+
+    memo[0] = 0
+    memo[1] = nums[0]
+
+    for (let i = 1; i < nums.length; i++) {
+        memo[i + 1] = Math.max(memo[i], memo[i - 1] + nums[i])
+    }
+
+    return memo[memo.length - 1]
+};
