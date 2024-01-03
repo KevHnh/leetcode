38f9de91ea9d1698956c4ac7a29db677f2280deb
Kevin Hinh <kevinhinh9@gmail.com>
2024-01-02 20:20:57 -0500

    Time: 243 ms (5.88%) | Memory: 52.3 MB (32.94%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.js
@@ -2,32 +2,8 @@
  * @param {string[]} bank
  * @return {number}
  */
-var numberOfBeams = function (bank) {
-    let ans = 0
-    let row = 0
-
-    for (let i = 0; i < bank.length; i++) {
-        let curr = bank[i].split("0").join("").length
-
-        if (curr !== 0 && i === 0) {
-            row = curr
-            continue
-        }
-
-        if (curr === 0) {
-            continue
-        }
-        else {
-            ans += row * curr
-            row = curr
-            curr = 0
-        }
-
-        console.log("ANS: ", ans)
-        console.log("ROW: ", row)
-        console.log("CURR: ", curr)
-        console.log("----------")
-    }
-
-    return ans
-};
+const numberOfBeams = (bank) =>
+    bank
+        .map((str) => str.split("0").join("").length)
+        .filter((val) => val !== 0)
+        .reduce((acc, cur, ind, arr) => acc + cur * (arr[ind + 1] || 0), 0);
