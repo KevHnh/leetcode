77466165c181002e0c93a6f7a1d9e2a516159a07
Kevin Hinh <kevinhinh9@gmail.com>
2024-01-10 01:57:26 -0500

    Time: 57 ms (47.33%) | Memory: 41.9 MB (88.93%) - LeetSync

--- 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.js
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.js
@@ -3,17 +3,11 @@
  * @return {number}
  */
 var minAddToMakeValid = function (s) {
-    let map = { ")": "(", "]": "[", "}": "{" }
     let stack = []
 
     for (let i = 0; i < s.length; i++) {
-        if (map[s[i]] != undefined) {
-            if (stack[stack.length - 1] === map[s[i]]) {
-                stack.pop()
-            }
-            else {
-                stack.push(s[i])
-            }
+        if (stack[stack.length - 1] === "(" && s[i] === ")") {
+            stack.pop()
         }
         else {
             stack.push(s[i])
