37d96ca7f5cc55d57a9a3b682aab6a95d961c3d1
Kevin Hinh <kevinhinh9@gmail.com>
2024-01-17 01:32:43 -0500

    Time: 63 ms (94.79%) | Memory: 60.7 MB (68.22%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    let num1 = Infinity
+    let num2 = Infinity
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > num2) {
+            return true
+        }
+
+        if (nums[i] > num1) {
+            num2 = nums[i]
+        }
+        else {
+            num1 = nums[i]
+        }
+    }
+
+    return false
+};
