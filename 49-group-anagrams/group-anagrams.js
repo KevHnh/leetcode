88b7e8d3055077f10314edd4604577081fcf5821
Kevin Hinh <kevinhinh9@gmail.com>
2024-02-05 22:47:59 -0500

    Time: 120 ms (41.78%) | Memory: 62.3 MB (23.45%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -3,20 +3,15 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
+    let sorted = strs.map(str => str.split("").sort().join(""))
     let map = {}
-    let sortedArr = []
 
     for (let i = 0; i < strs.length; i++) {
-        let sorted = strs[i].split("").sort().join("")
-        sortedArr.push(sorted)
-    }
-
-    for (let i = 0; i < strs.length; i++) {
-        if (map[sortedArr[i]] !== undefined) {
-            map[sortedArr[i]].push(strs[i])
+        if (map[sorted[i]]) {
+            map[sorted[i]].push(strs[i])
         }
         else {
-            map[sortedArr[i]] = [strs[i]]
+            map[sorted[i]] = [strs[i]]
         }
     }
 
