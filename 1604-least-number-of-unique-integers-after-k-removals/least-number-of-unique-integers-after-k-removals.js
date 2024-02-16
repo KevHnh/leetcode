a1b802d341f248cc448b1704460cfb19f471b1d7
Kevin Hinh <kevinhinh9@gmail.com>
2024-02-15 22:20:13 -0500

    Time: 91 ms (95.38%) | Memory: 71.4 MB (65.64%) - LeetSync

--- 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.js
+++ 1604-least-number-of-unique-integers-after-k-removals/least-number-of-unique-integers-after-k-removals.js
@@ -4,23 +4,17 @@
  * @return {number}
  */
 var findLeastNumOfUniqueInts = function (arr, k) {
-    let map = {}
-
-    for (let i = 0; i < arr.length; i++) {
-        map[arr[i]] ? map[arr[i]]++ : map[arr[i]] = 1
-    }
-
-    let cnt = Object.values(map).sort((a, b) => a - b)
-
-    while (k > 0) {
-        cnt[0]--
-
-        if (cnt[0] === 0) {
-            cnt.shift()
+    let m = new Map();
+    arr.forEach(num => m.set(num, m.get(num)+1 || 1));
+    let occurrences = Array.from(m.values());
+    occurrences.sort((a,b) => a-b);
+    let res = occurrences.length;
+    for (let num of occurrences) {
+        if (k >= num) {
+            k -= num;
+            res--;
         }
-
-        k--
+        else return res;
     }
-
-    return cnt.length
+    return res;
 };
