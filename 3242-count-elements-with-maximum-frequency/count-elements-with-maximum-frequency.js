d7d80cc03e372a8920760309e779e2f6053d5638
Kevin Hinh <kevinhinh9@gmail.com>
2024-03-07 21:23:25 -0500

    Time: 56 ms (84.20%) | Memory: 50.8 MB (50.43%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.js
@@ -2,20 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var maxFrequencyElements = function(nums) {
+var maxFrequencyElements = function (nums) {
     let map = {}
 
     for (let i = 0; i < nums.length; i++) {
         map[nums[i]] ? map[nums[i]]++ : map[nums[i]] = 1
     }
 
-    let sorted = Object.values(map).sort((a,b) => b - a)
+    let sorted = Object.values(map).sort((a, b) => b - a)
     let ans = 0
     let max = sorted[0]
 
     for (let i = 0; i < sorted.length; i++) {
         if (sorted[i] === max) {
             ans += sorted[i]
+        }
+        else {
+            break
         }
     }
 
