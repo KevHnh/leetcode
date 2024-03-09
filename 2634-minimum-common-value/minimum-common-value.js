721e81522ab10758c9decb6085a957708a6539aa
Kevin Hinh <kevinhinh9@gmail.com>
2024-03-08 22:49:40 -0500

    Time: 63 ms (86.99%) | Memory: 55.9 MB (92.35%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.js
+++ 2634-minimum-common-value/minimum-common-value.js
@@ -4,26 +4,18 @@
  * @return {number}
  */
 var getCommon = function (nums1, nums2) {
-    let map = {}
-    nums1 = Array.from(new Set(nums1))
-    nums2 = Array.from(new Set(nums2))
+    let left = 0;
+    let right = 0;
 
-    for (let i = 0; i < nums1.length; i++) {
-        map[nums1[i]] ? map[nums1[i]]++ : map[nums1[i]] = 1
-    }
-
-    for (let i = 0; i < nums2.length; i++) {
-        map[nums2[i]] ? map[nums2[i]]++ : map[nums2[i]] = 1
-    }
-
-    let sorted = Object.entries(map).sort((a,b) => a[0] - b[0])
-    console.log(sorted)
-
-    for (let i = 0; i < sorted.length; i++) {
-        if (sorted[i][1] >= 2) {
-            return sorted[i][0]
+    while (left < nums1.length && right < nums2.length) {
+        if (nums1[left] === nums2[right]) {
+            return nums1[left];
+        } else if (nums1[left] > nums2[right]) {
+            right++;
+        } else {
+            left++;
         }
     }
-
-    return -1
+    
+    return -1;
 };
