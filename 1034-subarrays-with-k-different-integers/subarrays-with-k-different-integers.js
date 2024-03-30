dc6755d156165931939d06aea6899f1f8b2f531a
Kevin Hinh <kevinhinh9@gmail.com>
2024-03-29 21:46:07 -0400

    Time: 61 ms (98.00%) | Memory: 56.4 MB (74.00%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.js
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.js
@@ -4,35 +4,34 @@
  * @return {number}
  */
 var subarraysWithKDistinct = function (nums, k) {
-    return (atMostK(nums, k) - atMostK(nums, k - 1));
-};
+    let n = nums.length;
+    let res = 0;
 
-function atMostK(A, K) {
-    let count = {};
-    let maxArr = 0, windowStart = 0;
+    let counters0 = new Uint16Array(20001);
+    let counters1 = new Uint16Array(20001);
 
-    for (let windowEnd = 0; windowEnd < A.length; windowEnd++) {
-        const rightInt = A[windowEnd];
+    counters0.fill(0, 0, n);
+    counters1.fill(0);
 
-        if (!(rightInt in count)) {
-            count[rightInt] = 0;
+    let c0 = 0;
+    let c1 = 0;
+    let r0 = -1;
+    let r1 = -1;
+
+    for (let l = 0; l < n; ++l) {
+        while (r0 < n && c0 < k) {
+            if (++r0 == n) break;
+            if (1 == ++counters0[nums[r0]]) ++c0;
+        }
+        while (r1 < n && c1 <= k) {
+            if (++r1 == n) break;
+            if (1 == ++counters1[nums[r1]]) ++c1;
         }
 
-        if (count[rightInt] === 0) {
-            K -= 1;
-        }
-        count[rightInt] += 1;
-
-        while (K < 0) {
-            const leftInt = A[windowStart];
-            count[leftInt] -= 1;
-            if (count[leftInt] === 0) {
-                K += 1;
-            }
-            windowStart += 1;
-        }
-        maxArr += windowEnd - windowStart + 1;
+        res += r1 - r0;
+        if (!--counters0[nums[l]]) --c0;
+        if (!--counters1[nums[l]]) --c1;
     }
 
-    return maxArr;
-}
+    return res;
+};
