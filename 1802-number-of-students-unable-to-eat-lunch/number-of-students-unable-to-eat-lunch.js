fd70357ab5bde32b9af196b975d56564a8ab6528
Kevin Hinh <kevinhinh9@gmail.com>
2024-04-07 22:17:18 -0400

    Time: 57 ms (38.73%) | Memory: 48.9 MB (62.28%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.js
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.js
@@ -4,24 +4,20 @@
  * @return {number}
  */
 var countStudents = function (students, sandwiches) {
-    let eat = 0
-    let totalStudents = students.length
-
     while (sandwiches.length > 0) {
         if (students[0] === sandwiches[0]) {
             students.shift()
             sandwiches.shift()
-            eat++
         }
         else {
             let temp = students.shift()
             students.push(temp)
 
             if (!students.includes(sandwiches[0])) {
-                return totalStudents - eat
+                return students.length
             }
         }
     }
 
-    return totalStudents - eat
+    return students.length
 };
