51d0d61dfbcf33dca24ba4ec2a1fc0d31bef38f4
Kevin Hinh <kevinhinh9@gmail.com>
2024-05-22 22:21:57 -0400

    Time: 393 ms (78.72%) | Memory: 53.5 MB (85.11%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var beautifulSubsets = function (nums, k) {
+    var bt = (idx, arr) => {
+        if (idx === nums.length) {
+            if (arr.length) return 1;
+            return 0;
+        }
+
+        let res = 0;
+
+        if (!arr.includes(nums[idx] - k) && !arr.includes(nums[idx] + k)) {
+            arr.push(nums[idx]);
+            res += bt(idx + 1, arr);
+            arr.pop();
+        }
+        
+        res += bt(idx + 1, arr);
+
+        return res;
+    }
+
+    return bt(0, []);
+};
