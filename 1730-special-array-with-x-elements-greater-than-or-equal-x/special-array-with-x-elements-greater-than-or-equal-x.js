5bc9fffc380fcdf77237768cf42900c02473f12b
Kevin Hinh <kevinhinh9@gmail.com>
2024-05-26 21:56:20 -0400

    Time: 63 ms (17.59%) | Memory: 48.8 MB (81.48%) - LeetSync

---
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var specialArray = function(nums) {
+    nums = nums.sort((a,b) => a - b)
+    let n = nums.length
+
+    let l = 0
+    let r = n - 1
+
+    while (l <= r) {
+        let mid = Math.floor((l + r) / 2)
+        let x = n - mid
+
+        if (nums[mid] >= x && (mid === 0 || nums[mid - 1] < x)) {
+            return x
+        }
+        else if (nums[mid] > x) {
+            r = mid - 1
+        }
+        else {
+            l = mid + 1
+        }
+    }
+
+    return -1
+};
