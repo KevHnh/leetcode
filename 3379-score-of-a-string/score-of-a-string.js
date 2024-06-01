6d218ce6ea695ef3c492796e1317c25e5ad18e0e
Kevin Hinh <kevinhinh9@gmail.com>
2024-05-31 21:33:11 -0400

    Time: 57 ms (74.49%) | Memory: 49.7 MB (66.55%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.js
+++ 3379-score-of-a-string/score-of-a-string.js
@@ -6,16 +6,7 @@ var scoreOfString = function(s) {
     let sum = 0
 
     for (let i = 1; i < s.length; i++) {
-        let curr = 0
-
-        if (i === 0) {
-            curr = Math.abs(s.charCodeAt(i) - s.charCodeAt(i + 1))
-        }
-        else {
-            curr = Math.abs(s.charCodeAt(i - 1) - s.charCodeAt(i))
-        }
-
-        sum += curr
+        sum += Math.abs(s.charCodeAt(i - 1) - s.charCodeAt(i))
     }
 
     return sum
