d64c960dd7c3de83a9a38b7251b57acd09daf84c
Kevin Hinh <kevinhinh9@gmail.com>
2024-06-06 20:15:20 -0400

    Time: 1237 ms (9.51%) | Memory: 57.3 MB (92.98%) - LeetSync

---
+++ 648-replace-words/replace-words.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} dictionary
+ * @param {string} sentence
+ * @return {string}
+ */
+var replaceWords = function (dictionary, sentence) {
+    let wordArr = sentence.split(" ")
+
+    wordArr = wordArr.map(w => {
+        for (let i = 0; i < dictionary.length; i++) {
+            if (w.indexOf(dictionary[i]) === 0) {
+                w = dictionary[i]
+            }
+        }
+
+        return w
+    })
+
+    return wordArr.reduce((str, word) => str += `${word} `, "").trim()
+};
