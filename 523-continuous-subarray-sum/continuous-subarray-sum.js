7276cb709ba2b099540fde83a1f369701b3e7347
Kevin Hinh <kevinhinh9@gmail.com>
2024-06-07 21:41:16 -0400

    Time: 88 ms (98.10%) | Memory: 73.3 MB (54.29%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.js
+++ 523-continuous-subarray-sum/continuous-subarray-sum.js
@@ -4,20 +4,19 @@
  * @return {boolean}
  */
 var checkSubarraySum = function (nums, k) {
-    let cumulative = 0;
-    
-    const map = { '0': -1 };
+    let store = new Set();
+    let sum = 0, prev = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        cumulative += nums[i];
+        sum += nums[i];
+        let r = sum % k;
 
-        if (map[cumulative % k] === undefined) {
-            map[cumulative % k] = i;
-        }
-
-        if (map[cumulative % k] < i - 1) {
+        if (store.has(r)) {
             return true;
         }
+        
+        store.add(prev);
+        prev = r;
     }
 
     return false;
