4e17fca9f8984f058aa0b2dba9f9de1bb9a0dee5
Kevin Hinh <kevinhinh9@gmail.com>
2024-06-26 21:05:13 -0400

    Time: 90 ms (48.94%) | Memory: 64.3 MB (63.12%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.js
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.js
@@ -2,15 +2,20 @@
  * @param {number[][]} edges
  * @return {number}
  */
-var findCenter = function(edges) {
-    let map = {}
+var findCenter = function (edges) {
+    let hash = {}
 
-    for (let i = 0; i < edges.length; i++) {
-        map[edges[i][0]] ? map[edges[i][0]]++ : map[edges[i][0]] = 1 
-        map[edges[i][1]] ? map[edges[i][1]]++ : map[edges[i][1]] = 1 
-    }
+    for (let [v1, v2] of edges) {
+        if (!hash[v1]) {
+            hash[v1] = 1
+        } else {
+            return v1
+        }
 
-    let center = Object.entries(map).sort((a,b) => b[1] - a[1])
-
-    return center[0][0]
-};
+        if (!hash[v2]) {
+            hash[v2] = 1
+        } else {
+            return v2
+        }
+    };
+}
