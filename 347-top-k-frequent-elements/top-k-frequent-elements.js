ce63f0d858c549fa2954c87e103292978bb4ee5c
Kevin Hinh <kevinhinh9@gmail.com>
2025-05-14 01:18:37 -0400

    Time: 13 ms (53.83%) | Memory: 58.3 MB (79.13%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -3,19 +3,14 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
+var topKFrequent = function (nums, k) {
     let map = {}
 
     for (let i = 0; i < nums.length; i++) {
         map[nums[i]] ? map[nums[i]]++ : map[nums[i]] = 1
     }
 
-    let sorted = Object.entries(map).sort((a,b) => b[1] - a[1])
-    let ans = []
+    let sorted = Object.entries(map).sort((a, b) => b[1] - a[1])
 
-    for (let i = 0; i < k; i++) {
-        ans.push(Number(sorted[i][0]))
-    }
-
-    return ans
+    return sorted.slice(0, k).map((entry) => Number(entry[0]))
 };
